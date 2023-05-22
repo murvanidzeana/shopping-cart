1a4e044989e5f07d87cc45724f6562a85bf8f00d
@muru <118741835+murvanidzeana@users.noreply.github.com>
2023-05-23 01:16:02 +0400

    shopping1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import Shopping from './components/shopping';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Shopping type="Nike"/>,
+    <Shopping type="HM"/>
+    
+  
+   
+
+    
   );
 }
 
